19133a2e70d9a8abd15a6c3cc26d63baed8c6b8f
Isaac Abraham <Isaac Abraham>
2016-12-10 00:36:35 +0530

    Included markers but cannot seem to enable streetview

--- js/app.js
+++ js/app.js
@@ -1,20 +1,86 @@
 var map;
+var markers = [];
 
 function initMap(){
-		map = new google.maps.Map(document.getElementById('map'),{
-	        center:{lat: 40.712784, lng: -74.005941},
-	        zoom: 10
-	       });
-	};
+	map = new google.maps.Map(document.getElementById('map'),{
+    center:{lat: 40.712784, lng: -74.005941},
+    zoom: 10
+    });
+
+    var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+
+	for (var i = 0; i < nycLocations.length; i++) {
+		var position = nycLocations[i].latlng;
+		var title = nycLocations[i].name;
+
+
+		var marker = new google.maps.Marker({
+        	map: map,
+        	position: position,
+        	title: title,
+        	animation: google.maps.Animation.DROP,
+        	id: i
+      	});
+      	// Push the marker to our array of markers.
+     	markers.push(marker);
+      	// Create an onclick event to open an infowindow at each marker.
+      	marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+      	});
+      	bounds.extend(markers[i].position);
+	}
+	map.fitBounds(bounds);
+};
+
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('');
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick',function(){
+        infowindow.setMarker(null);
+      });
+      var streetViewService = new google.maps.StreetViewService();
+      var radius = 50;
+      // In case the status is OK, which means the pano was found, compute the
+      // position of the streetview image, then calculate the heading, then get a
+      // panorama from that and set the options
+      function getStreetView(data, status) {
+        if (status == google.maps.StreetViewStatus.OK) {
+          var nearStreetViewLocation = data.location.latLng;
+          var heading = google.maps.geometry.spherical.computeHeading(
+            nearStreetViewLocation, marker.position);
+            infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+            var panoramaOptions = {
+              position: nearStreetViewLocation,
+              pov: {
+                heading: heading,
+                pitch: 30
+              }
+            };
+          var panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('pano'), panoramaOptions);
+        } else {
+          infowindow.setContent('<div>' + marker.title + '</div>' +
+            '<div>No Street View Found</div>');
+        }
+      }
+      // Use streetview service to get the closest streetview image within
+      // 50 meters of the markers position
+      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+      // Open the infowindow on the correct marker.
+      infowindow.open(map, marker);
+    }
+ }
+
+
 
 var ViewModel = function(){
-	var self = this;
-	this.markerLoc = ko.observableArray([]);
-
-	nycLocations.forEach(function(nycLoc){
-		self.markerLoc.push(nycLoc);
-	});
-
+	
+}
 	
 
 ko.applyBindings(new ViewModel());
--- js/model.js
+++ js/model.js
@@ -1,68 +1,57 @@
 var nycLocations = [
 	{
 		name: 'Times Square',
-		lat: '40.759011',
-		lng: '-73.984472',
+		latlng: {lat:40.759011, lng:-73.984472},
 		address: 'Manhattan,New York, NY 10036, USA'
 	},
 	{
 		name: 'Irving PLaza',
-		lat: '40.736778',
-		lng: '-73.984472',
+		latlng: {lat: 40.736778, lng: -73.984472},
 		address: '17 Irving Pl, New York, Ny 10063, USA'
 	},
 	{
 		name: 'Central Park',
-		lat: '40.771133',
-		lng: '-73.974187',
+		latlng: {lat: 40.771133, lng: -73.974187},
 		address: 'New York, NY 10024, USA'
 	},
 	{
 		name: '30 Rockerfeller Plaza',
-		lat: '40.758963',
-		lng: '-73.979337',
+		latlng: {lat: 40.758963, lng: -73.979337},
 		address: 'New York, NY 10112, USA'
 	},
 	{
 		name: 'McGees Pub',
-		lat: '40.712784',
-		lng: '-74.005941',
+		latlng: {lat: 40.712784, lng: -74.005941},
 		address: '240 W 55th St, New York, NY 10019, USA'
 	},
 	{
 		name: 'Pulitzer Fountain',
-		lat: '40.712784',
-		lng: '-74.005941',
+		latlng: {lat: 40.712784, lng: -74.005941},
 		address: 'New York, NY 10019, USA'
 	},
 	{
 		name: 'Central Perk Cafe',
-		lat: '40.712784',
-		lng: '-74.005941',
+		latlng: {lat: 40.712784, lng: -74.005941},
 		address: '105 Cedarhust Ave, Cedarhust, NY 11516, USA'
 	},
 	{
 		name: 'Empire State Building',
-		lat: '40.748441',
-		lng: '-73.985664',
+		latlng: {lat: 40.748441, lng: -73.985664},
 		address: '350 5th Avenue, New York ,NY 10118, USA'
 	},
 	{
 		name: 'Corner Bistro',
-		lat: '40.738012',
-		lng: '-74.003782',
+		latlng: {lat: 40.738012, lng: -74.003782},
 		address: '331 West 4th Street, New York, NY 10014, USA'
 	},
 	{
 		name: 'Indoor Extreme Sports Paintball and Lasertag',
-		lat: '40.742098',
-		lng: '-73.933701',
+		latlng: {lat: 40.742098, lng: -73.933701},
 		address: '47-11 Van Dam St, Long Island, NY 11101, USA'
 	},
 	{
 		name: 'American Museum of Natural History',
-		lat: '40.781324',
-		lng: '-73.973958',
+		latlng: {lat: 40.781324, lng: -73.973958},
 		address: 'Central Park West & 79th St, New York, NY 10024, USA'
 	},
 ]
